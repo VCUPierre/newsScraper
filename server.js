f8c538f69305f7b1353c9b0abcff8339e6394335
Tayvon Pierre Randall <PierreMac@vcupierre.lan>
2019-12-14 22:57:21 -0500

    fixed port for Heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.Port || 3000;
 
 // Initialize Express
 var app = express();
